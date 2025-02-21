92dca7a70398d98bea487cbf8c16d897d53c0af1
AnastasiayaKuba <anastasia.kubanskaia.01@gmail.com>
2025-02-22 01:29:37 +0300

    Add files via upload

---
+++ Task-6/src/main/java/web/model/Car.java
@@ -0,0 +1,49 @@
+package web.model;
+
+public class Car {
+    private long id;
+    private String color;
+    private String model;
+    private int age;
+
+    public Car(String model, String color, int age) {
+        this.model = model;
+        this.color = color;
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    public Car() {
+
+    }
+}
---
+++ Task-6/src/main/java/web/service/CarService.java
@@ -0,0 +1,28 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarService {
+
+    private List<Car> cars;
+    public CarService() {
+        cars  = new ArrayList<>();
+        cars.add(new Car("Toyota", "white", 2010));
+        cars.add(new Car("Volkswagen", "grey", 2019));
+        cars.add(new Car("Lada", "black", 2012));
+        cars.add(new Car("BMW", "blue", 2024));
+        cars.add(new Car("Mercedes", "red", 1998));
+    }
+
+    public List<Car> getCars(int count) {
+        if (count > 5) {
+            return cars;
+        }
+        return cars.subList(0, count);
+    }
+}
